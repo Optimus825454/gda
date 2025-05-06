33e7833cb1f990271f947447df13bf3269a11f60
optmus825454 <ikinciyenikitap54@gmail.com>
2025-05-06 22:10:22 +0300

    UserController import düzeltildi

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const UserController = require('../controllers/userController');
+const UserController = require('../controllers/UserController');
 const { checkPermission } = require('../middleware/checkPermission');
 const authMiddleware = require('../middleware/authMiddleware');
 
